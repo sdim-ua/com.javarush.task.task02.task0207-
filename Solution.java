8f08a171902870931a93eed21b96d5beef0624a3
sdim-ua <ingersol@yandex.ua>
2018-12-18 00:38:43 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+/* 
+Кусочек калькулятора
+*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        
+         int a = 5; 
+         int b = 12; 
+         System.out.println(a + b); 
+         System.out.println(a * b); 
+    }
+}
